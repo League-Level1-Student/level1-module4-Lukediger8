01d88b2ddb4389796b4ccccac1a8a2cab01639f9
root <root@4e0e5eeb07c0>
2023-01-21 20:48:40 +0000

    e

---
+++ src/_01_chuckle_clicker/code.java
@@ -0,0 +1,56 @@
+package _01_chuckle_clicker;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class code implements ActionListener {
+	JFrame frame;
+	JPanel panel;
+	JButton jokeButton;
+	JButton punchlineButton;
+	public static void main(String[] args) {
+		code chuckleClicker = new code();
+		chuckleClicker.MakeButtons();
+	}
+	
+	void MakeButtons()
+	{
+		frame = new JFrame();
+		
+		frame.setVisible(true);
+		
+		panel = new JPanel();
+		
+		jokeButton = new JButton();
+		jokeButton.setText("Joke");
+		jokeButton.addActionListener(this);
+		punchlineButton = new JButton();
+		punchlineButton.setText("Punchline");
+		punchlineButton.addActionListener(this);
+		
+		frame.add(panel);
+		panel.add(jokeButton);
+		panel.add(punchlineButton);
+		
+		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == jokeButton)
+		{
+			JOptionPane.showMessageDialog(null, "Why did the chicken cross the road?");
+		}
+		else if (e.getSource() == punchlineButton)
+		{
+			JOptionPane.showMessageDialog(null, "to get to the other side.");
+		}
+	}
+	
+	
+}
