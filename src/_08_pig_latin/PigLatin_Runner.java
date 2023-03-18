00a32d8310425063dcd33bc3057ff6b2c07a59f6
root <root@07b815d4c197>
2023-03-18 19:47:50 +0000

    jojo *Jojo theme*

---
+++ src/_08_pig_latin/PigLatin_Runner.java
@@ -0,0 +1,23 @@
+package _08_pig_latin;
+
+public class PigLatin_Runner {
+
+	public static void main(String[] args) {
+		
+	PigLatinTranslator t = new PigLatinTranslator();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
