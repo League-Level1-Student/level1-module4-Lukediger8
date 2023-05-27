c7ece0a4e680f0b038b25f36ea101922c1d60e95
root <root@44588f74b83f>
2023-05-27 19:54:46 +0000

    e

--- src/_09_whack_a_mole/whack_a_mole.java
+++ src/_09_whack_a_mole/whack_a_mole.java
@@ -1,19 +1,32 @@
 package _09_whack_a_mole;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class whack_a_mole {
+public class whack_a_mole implements MouseListener {
 	public static void main(String[] args) {
 
+	
+		
+	}
+	
+
+	
+	
+
+	void stuff() {
 		JFrame frame = new JFrame();
 
 		JPanel panel = new JPanel();
-
+		
+		frame.addMouseListener(this);
 		int f= 90;
 		int g= 40;
 
@@ -46,27 +59,40 @@ public class whack_a_mole {
 				
 			}	
 		}
-		
-	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
