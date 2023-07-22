921718a3f86e0fec3f2f40b281a80a8f30592cb4
root <root@edfa83ffa73c>
2023-07-22 19:52:59 +0000

    e

---
+++ src/_10_slot_machine/slot_machine.java
@@ -0,0 +1,36 @@
+package _10_slot_machine;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class slot_machine {
+JButton SPIN = new JButton();
+JPanel panel = new JPanel();	
+JFrame frame = new JFrame();
+	private JLabel createLabelImage(String fileName) throws MalformedURLException{
+        URL imageURL = getClass().getResource(fileName);
+	if (imageURL == null){
+		System.err.println("Could not find image " + fileName);
+		return new JLabel();
+		}
+	}
+
+void stuff() {
+int f= 90;
+int g=40;
+	
+SPIN.addActionListener(l);
+frame.add(panel);
+panel.add(SPIN);
+frame.setSize(600,558);
+panel.setSize(600,558);
+SPIN.setSize(f,g);
+
+}
+}
